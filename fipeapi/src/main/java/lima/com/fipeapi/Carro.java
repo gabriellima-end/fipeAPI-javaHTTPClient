0a196f9dc7ebf22a4a83338b0083f4a346cf0519
Gabriel Lima <106500760+gabriellima-end@users.noreply.github.com>
2025-04-11 11:15:17 -0300

    Add files via upload

---
+++ fipeapi/src/main/java/lima/com/fipeapi/Carro.java
@@ -0,0 +1,48 @@
+package lima.com.fipeapi;
+
+public class Carro {
+	
+	private String marca;
+	private String modelo;
+	private int ano;
+	
+	
+	
+	//GETTERS E SETTERS ---------
+	public String getMarca() {
+		
+		return this.marca;
+		
+	}
+	
+	public void setMarca(String marca) {
+		
+		this.marca = marca;
+		
+	}
+	
+	public String getModelo() {
+		
+		return this.modelo;
+		
+	}
+	
+	public void setModelo(String modelo) {
+		
+		this.modelo = modelo;
+		
+	}
+	
+	public int getAno() {
+		
+		return this.ano;
+		
+	}
+	
+	public void setAno(int ano) {
+		
+		this.ano = ano;
+		
+	}
+	
+}
---
+++ fipeapi/src/main/java/lima/com/fipeapi/ConnectionAPI.java
@@ -0,0 +1,16 @@
+package lima.com.fipeapi;
+
+
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.IOException;
+
+
+public class ConnectionAPI {
+	
+	
+	
+}
